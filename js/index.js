eefc6d6c71df332aaf2d393cda61dee380959631
Ryan Zola <ryanzola@me.com>
2021-03-04 02:43:24 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,161 @@
+import * as THREE from "three";
+
+import fragment from "./shader/fragment.glsl";
+import vertex from "./shader/vertex.glsl";
+
+import matcap1 from "../assets/matcap1.jpg";
+import matcap2 from "../assets/matcap2.jpg";
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.mouse = new THREE.Vector2();
+
+    this.container.appendChild(this.renderer.domElement);
+
+    var frustumSize = 1;
+    var aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.OrthographicCamera(
+      frustumSize / -2,
+      frustumSize / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      -1000,
+      1000
+    );
+    this.camera.position.set(0, 0, 2);
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
+
+    this.isPlaying = true;
+
+    this.addObjects();
+    this.resize();
+    this.render();
+    this.setupResize();
+    this.mouseEvents();
+    this.settings();
+  }
+
+  settings() {
+    this.settings = {
+      progress: 0
+    };
+    // this.gui = new dat.GUI();
+    // this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    // image cover
+    this.imageAspect = 1;
+    let a1, a2;
+
+    if (this.height / this.width > this.imageAspect) {
+      a1 = (this.width / this.height) * this.imageAspect;
+      a2 = 1;
+    } else {
+      a1 = 1;
+      a2 = (this.height / this.width) * this.imageAspect;
+    }
+
+    this.material.uniforms.resolution.value.x = this.width;
+    this.material.uniforms.resolution.value.y = this.height;
+    this.material.uniforms.resolution.value.z = a1;
+    this.material.uniforms.resolution.value.w = a2;
+
+    const dist = this.camera.position.z;
+    const height = 1;
+
+    this.camera.fov = 2 * (180 / Math.PI) * Math.atan(height / (2 * dist));
+
+    if (this.width / this.height > 1) {
+      this.plane.scale.x = this.camera.aspect;
+    } else {
+      this.plane.scale.y = 1 / this.camera.aspect;
+    }
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  mouseEvents() {
+    document.addEventListener("mousemove", (e) => {
+      this.mouse.x = e.pageX / this.width - 0.5;
+      this.mouse.y = -e.pageY / this.height + 0.5;
+    });
+  }
+
+  addObjects() {
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable"
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { type: "f", value: 0 },
+        progress: { type: "f", value: 0 },
+        mouse: { type: "v2", value: new THREE.Vector2() },
+        matcap: { type: "t", value: new THREE.TextureLoader().load(matcap1) },
+        matcap2: { type: "t", value: new THREE.TextureLoader().load(matcap2) },
+        resolution: { type: "v4", value: new THREE.Vector4() },
+        uvRate1: {
+          value: new THREE.Vector2(1, 1)
+        }
+      },
+      // wireframe: true,
+      // transparent: true,
+      vertexShader: vertex,
+      fragmentShader: fragment
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.render();
+      this.isPlaying = true;
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    this.material.uniforms.progress.value = this.settings.progress;
+    if (this.mouse) {
+      this.material.uniforms.mouse.value = this.mouse;
+    }
+
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container")
+});
